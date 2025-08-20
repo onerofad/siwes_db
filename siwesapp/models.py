28b2fa6bb137c31b09d9d3ba9375ff402c8d802e
frank <frank@gmail.com>
2025-08-20 12:30:20 -0700

    first commit

--- siwesapp/models.py
+++ siwesapp/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Students(models.Model):
+    firstname = models.CharField(max_length=255, default="")
+    middlename = models.CharField(max_length=255, default="")
+    lastname = models.CharField(max_length=255, default="")
     matricno = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.matricno
