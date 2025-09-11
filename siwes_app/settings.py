9dd2f5347976e8aa4b9324f5598775619fba5044
unknown <onerofad>
2025-09-11 15:51:28 +0100

    first commit

--- siwes_app/settings.py
+++ siwes_app/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'siwes_app.wsgi.application'
 #}
 
 DATABASES = {
-'default': dj_database_url.parse(f'postgresql://postgres:YHXqXvZbJoGrJoEMIHBHcblytUuzKAmU@centerbeam.proxy.rlwy.net:27642/railway')
+'default': dj_database_url.parse(f'postgresql://postgres:DisqsBgIcLNLwrXWLmQUCtlvjxJXyolR@centerbeam.proxy.rlwy.net:19478/railway')
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
