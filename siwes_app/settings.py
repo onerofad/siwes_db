1a8b99cfdc9bf0a888320c829a5f6b258101156e
frank <frank@gmail.com>
2026-01-08 04:21:53 -0800

    first commit

--- siwes_app/settings.py
+++ siwes_app/settings.py
@@ -137,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
+    'http://localhost:3001',
     'https://siwes.v5s.co.uk',
     'https://siwes.delsutestserver.ng',
     'https://siwes-two.vercel.app',
